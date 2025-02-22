fb6adf8fed3d7e40ebef2747f2c4a6f3b8cf64ae
Sriparna Koar <koarsk03@gmail.com>
2025-02-22 15:31:53 +0530

    first commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true},
   companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
   role: { type: String, default: 'employee' },
 });
